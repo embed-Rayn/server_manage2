76b553348241c627d55dc878b9398fd7df346e83
embed-Rayn <57181674+embed-Rayn@users.noreply.github.com>
2025-06-18 17:27:08 +0900

    update dyanamic jupyter password

--- 1. 관리자 메뉴얼/jupyter_lab_config.py
+++ 1. 관리자 메뉴얼/jupyter_lab_config.py
@@ -1,6 +1,20 @@
 # Configuration file for lab.
 
-c = get_config()  #noqa
+c = get_config()
+c.ServerApp.ip = "0.0.0.0"
+c.ServerApp.allow_root = True
+c.ServerApp.open_browser = False
+
+import os
+from jupyter_server.auth.security import passwd
+
+# 환경변수에서 비밀번호를 읽어서 해시. 빈 값이면 토큰 인증 유지
+raw_pw = os.environ.get("JUPYTER_PASSWORD", "")
+c.ServerApp.token = ""
+if raw_pw:
+    c.ServerApp.password = passwd(raw_pw)
+else:
+    c.ServerApp.password = passwd("default_123")
 
 #------------------------------------------------------------------------------
 # Application(SingletonConfigurable) configuration
